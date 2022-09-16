28cfa14f16a5b3b1708b8a403ad4ca914386fb42
natainditama <natainditama@gmail.com>
2022-09-16 20:51:19 +0800

    chore: Add navbar component

---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="absolute inset-0 bottom-auto h-20 lg:left-20">
+      <div className="container mx-auto bg-blue-400 h-full">Navbar</div>
+    </nav>
+  );
+}
+
+export default Navbar;
