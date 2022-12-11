333b7e0dffcf73cf53cfaf07659706dab4e5899d
natainditama <natainditama@gmail.com>
2022-12-11 14:28:02 +0800

    chore: Make the website's main layout.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,34 +1,39 @@
 import { useRouter } from "next/router";
 import type { AppProps } from "next/app";
 import { useRef } from "react";
-import { LocomotiveScrollProvider as RLSProvider } from "react-locomotive-scroll";
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
 import "locomotive-scroll/dist/locomotive-scroll.css";
-import "../styles/globals.css";
+import Header from "@components/header/Header";
+import Layout from "@components/layout/Layout";
+import "@styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const { asPath } = useRouter();
-  const containerRef = useRef(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+
   return (
-    <RLSProvider
-      options={{
-        smooth: true,
-        // ... all available Locomotive Scroll instance options
-      }}
-      watch={
-        [
-          //..all the dependencies you want to watch to update the scroll.
-          //  Basicaly, you would want to watch page/location changes
-          //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
-        ]
-      }
-      location={asPath}
-      onLocationChange={(scroll: any) => scroll.scrollTo(0, { duration: 0, disableLerp: true })}
-      containerRef={containerRef}
-    >
-      <div data-scroll-container ref={containerRef}>
-        <Component {...pageProps} />;
-      </div>
-    </RLSProvider>
+    <>
+      <LocomotiveScrollProvider
+        options={{
+          smooth: true,
+          smartphone: {
+            smooth: true,
+          },
+          scrollbarClass: "scrollbar",
+        }}
+        watch={[]}
+        location={asPath}
+        onLocationChange={(scroll: any) => scroll.scrollTo("top", { disableLerp: true })}
+        containerRef={containerRef}
+      >
+        <Header />
+        <main data-scroll-container ref={containerRef}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </main>
+      </LocomotiveScrollProvider>
+    </>
   );
 }
 
