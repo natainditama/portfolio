bdd4a9f2edbd0924faa00e0c6eb813e86483769a
natainditama <natainditama@gmail.com>
2022-12-12 20:29:09 +0800

    Implement typed.js libraries for header

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,13 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import Link from "next/link";
+import Typed from "typed.js";
 import Image from "next/image";
 
 function Header() {
+  const typed = useRef<Typed | null>(null);
+  const typedElem = useRef<HTMLParagraphElement>(null);
+
+  useEffect(function () {
+    if (typedElem.current) {
+      typed.current = new Typed(typedElem.current, {
+        strings: [
+          "Software Developer",
+          "Software Engineering",
+          "Front-end Developer",
+          "Freelancer",
+        ],
+        backDelay: 3500,
+        typeSpeed: 50,
+        loop: true,
+        showCursor: false,
+      });
+    }
+    return function () {
+      typed.current && typed.current.destroy();
+    };
+  }, []);
+
   return (
     <header className="fixed inset-0 bottom-auto z-40 min-h-[80px] bg-white/80 shadow-sm backdrop-blur-sm">
       <div className="container mx-auto flex h-full min-h-[80px] items-center justify-between">
         <div className="flex flex-row items-center md:space-x-2">
-          <nav className="items-center justify-between hidden md:flex md:w-auto md:space-x-5">
+          <div className="flex flex-col items-start justify-center" data-cursor="-opaque">
+            <Link href={"/"}>
+              <h2 className="cursor-pointer text-xl">Nata Inditama</h2>
+            </Link>
+            <p className="min-h-[1rem] overflow-hidden text-sm opacity-90" ref={typedElem}></p>
+          </div>
+          {/* <nav className="hidden items-center justify-between md:flex md:w-auto md:space-x-5">
             <Link href="/">
               <a className="nav-link nav-link-grow-up">
                 <span>Home</span>
@@ -26,7 +56,7 @@ function Header() {
                 <span>Contact</span>
               </a>
             </Link>
-          </nav>
+          </nav> */}
         </div>
         <div className="flex space-x-4">
           {/* <button
