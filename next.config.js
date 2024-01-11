1d8e37373c9f29ba8333f1d561d8ff4c079e7d21
Nata Inditama <natainditama.dev@gmail.com>
2024-01-11 15:05:46 +0800

    chore: configure shadcn-ui

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withBundleAnalyzer = require("@next/bundle-analyzer");
+const withTM = require("next-transpile-modules");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: true,
+  images: {
+    formats: ["image/avif", "image/webp"],
+  },
+  experimental: {},
+};
+
+const nextPlugins = [
+  withBundleAnalyzer({ enabled: process.env.ANALYZE === "true" }),
+  withTM([]),
+];
+
+module.exports = (_phase, { defaultConfig: _ }) => {
+  return nextPlugins.reduce((acc, plugin) => plugin(acc), { ...nextConfig });
+};
